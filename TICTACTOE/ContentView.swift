f19d1cfce7c2554ec8c8ad5ec5875db8d90176d9
a.m.student <tailyrdebose2017@gmail.com>
2021-01-28 09:45:13 -0600

    Initial Commit

---
+++ TICTACTOE/ContentView.swift
@@ -0,0 +1,159 @@
+//
+//  ContentView.swift
+//  tictactoe2
+//
+//  Created by A.M. Student on 1/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        NavigationView {
+            Home()
+                .navigationTitle("Tic Tac Toe")
+                .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+struct Home: View {
+    
+    @State var moves : [String] = Array(repeating: "", count: 9)
+    
+    @State var isPlaying = true
+    @State var gameOver = false
+    @State var msg = ""
+    
+    var body: some View {
+        VStack {
+            
+            LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 15), count: 3), spacing: 15) {
+                
+                ForEach(0..<9, id: \.self) {index in
+                    
+                    ZStack {
+                        Color.pink
+                        
+                        Color.purple
+                            .opacity(moves[index] == "" ? 1 : 0)
+                        
+                        Text(moves[index])
+                            .font(.system(size: 55))
+                            .fontWeight(.heavy)
+                            .foregroundColor(.black)
+                    }
+                    
+                    .frame(width: getWidth(), height:
+                            getWidth())
+                    .cornerRadius(15)
+                    .rotation3DEffect(
+                        .init(degrees: moves[index] != "" ? 180 : 0),
+                        axis: /*@START_MENU_TOKEN@*/(x: 0.0, y: 1.0, z: 0.0)/*@END_MENU_TOKEN@*/,
+                        anchor: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/,
+                        anchorZ: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/,
+                        perspective: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/
+                    )
+                    
+                    .onTapGesture(perform: {
+                        withAnimation(Animation
+                                        .easeIn(duration: 1.0)) {
+                            
+                            if moves[index] == "" {
+                                moves[index] = isPlaying ? "X" : "O"
+                                isPlaying.toggle()
+                            }
+                        }
+                    })
+                }
+            }
+            .padding(15)
+        }
+        //check for winner
+        .onChange(of: moves, perform: { value in
+            checkWinner()
+        })
+        .alert(isPresented: $gameOver, content: {
+            
+            Alert(title: Text("Winner"), message: Text(msg), dismissButton: .destructive(Text( "Play Again?"), action: {
+                
+                withAnimation(Animation.easeIn(duration: 0.5)) {
+                    moves.removeAll()
+                    moves = Array(repeating: "", count: 9)
+                    isPlaying = true
+                }
+                
+            }))
+        })
+        
+    }
+    
+    func getWidth() -> CGFloat {
+        let width = UIScreen.main.bounds.width - (30 + 30)
+        
+        return width / 3
+    }
+    
+    func checkWinner() {
+        if checkMoves (player: "X") {
+            msg = "Player X Won"
+            gameOver.toggle()
+        }
+        if checkMoves (player: "O") {
+            msg = "Player 0 won"
+            gameOver.toggle()
+        } else {
+            let status = moves.contains { (value) -> Bool in
+                return value == ""
+                
+            }
+            if !status {
+                msg = "Game Over Tied!"
+                gameOver.toggle()
+            }
+        }
+        
+        
+        
+    }
+    
+    
+    func checkMoves(player: String) -> Bool {
+        //horizontal moves check
+        for contestant in stride(from:  0, to: 9, by: 3) {
+            if moves [contestant] == player && moves [contestant + 1] == player && moves[contestant + 2] == player {
+                return true
+            }
+        }
+        
+        //vertical moves check
+        
+        for contestant in 0...2 {
+            if moves [contestant] == player && moves [contestant + 3] == player && moves[contestant + 6] == player {
+                return true
+            }
+            
+        }
+        
+        //diagnoal moves check
+        if moves [0] == player && moves [4] == player && moves[8] == player {
+            return true
+        }
+        if moves [2] == player && moves [4] == player && moves[6] == player {
+            return true
+        }
+        
+        
+        return false
+    }
+    
+    
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+}
